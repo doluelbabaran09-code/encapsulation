6975f4d8f2ae9ddc721f8a1a16824ed6167e6358
doluelbabaran09 <dolueelbabaran09@gmail.com>
2026-06-05 02:09:06 +0800

    add accessor methods for car attributes

--- car/car_simulator.py
+++ car/car_simulator.py
@@ -13,3 +13,9 @@ class Car:
         else:
             self.__speed = 0
     
+    def get_speed(self):
+        return self.__speed
+    def get_year_model(self):
+        return self.__year_model
+    def get_make(self):
+        return self.__make
