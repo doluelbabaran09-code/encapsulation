f5214cec829091450a91da3ab161652097c0186b
doluelbabaran09 <dolueelbabaran09@gmail.com>
2026-06-05 03:03:21 +0800

    created the mutator methods for name, type, and age

--- pets/pet_manager.py
+++ pets/pet_manager.py
@@ -1,6 +1,12 @@
 class Pets:
-    def __init__(self)
+    def __init__(self):
         self.__name = ""
         self.__animal_type = ""
         self.__age = 0
-    
+    def set_name(self, name: str):
+        self.__name = name
+    def set_animal_type(self, animal_type: str):
+        self.__animal_type = animal_type
+    def set_age(self, age: int):
+        self.__age = age
+ 
