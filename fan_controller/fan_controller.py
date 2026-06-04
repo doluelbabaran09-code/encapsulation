873c23ccd146c23228c156acbe92573167fdc0d2
doluelbabaran09 <dolueelbabaran09@gmail.com>
2026-06-04 21:32:56 +0800

    implement getter and setter methods

--- fan_controller/fan_controller.py
+++ fan_controller/fan_controller.py
@@ -1,4 +1,4 @@
-class fan:
+class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST =  3
@@ -7,4 +7,21 @@ class fan:
         self.__is_on = on
         self.__radius = float(radius)
         self.__color = color
-        
+
+    def get_speed(self):
+        return self.__speed
+    def get_is_on(self):
+        return self.__is_on
+    def get_radius(self):
+        return self.__radius
+    def get_color(self):
+        return self.__color
+    
+    def set_speed(self, speed: int):
+        self.__speed= speed
+    def set_is_on(self, on: bool):
+        self.__is_on = on
+    def set_radius(self, radius: float):
+        self.__radius = radius
+    def set_color(self, color: str):
+        self.__color = color
