94aab1232ab6bc95b81ce0ea318d16e315d5e445
doluelbabaran09 <dolueelbabaran09@gmail.com>
2026-06-05 02:06:46 +0800

    created the accelerate and brake mutator methods

--- car/car_simulator.py
+++ car/car_simulator.py
@@ -4,3 +4,12 @@ class Car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0 
+    
+    def accelerate(self):
+        self.__speed += 5
+    def brake(self):
+        self.__speed -= 5
+        if self.__speed < 0:
+        else:
+            self.__speed = 0
+    
